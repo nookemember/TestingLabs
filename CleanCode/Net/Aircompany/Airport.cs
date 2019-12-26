83c14f5d5ce49540dc97b184c660a2f413ed96d0
Nikita Yaskovets <nooker576@gmail.com>
2019-12-26 18:36:42 +0300

    Delete trash files

---
+++ CleanCode/Net/Aircompany/Airport.cs
@@ -0,0 +1,94 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        private readonly List<Plane> _planes;
+
+        public Airport(IEnumerable<Plane> planes)
+        {
+            _planes = planes.ToList();
+        }
+
+        public List<PassengerPlane> GetPassengersPlanes()
+        {
+            List<PassengerPlane> passengerPlanes = new List<PassengerPlane>();
+            for (int i = 0; i < _planes.Count; i++)
+            {
+                if (_planes[i].GetType() == typeof(PassengerPlane))
+                {
+                    passengerPlanes.Add((PassengerPlane)_planes[i]);
+                }
+            }
+            return passengerPlanes;
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanes()
+        {
+            List<MilitaryPlane> militaryPlanes = new List<MilitaryPlane>();
+            for (int i = 0; i < _planes.Count; i++)
+            {
+                if (_planes[i].GetType() == typeof(MilitaryPlane))
+                {
+                    militaryPlanes.Add((MilitaryPlane)_planes[i]);
+                }
+            }
+            return militaryPlanes;
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            List<PassengerPlane> passengerPlanes = GetPassengersPlanes();
+            return passengerPlanes.Aggregate((plane1, plane2) => plane1.GetPassengersCapacity() > plane2.GetPassengersCapacity() ? plane1 : plane2);             
+        }
+
+        public List<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            List<MilitaryPlane> transportMilitaryPlanes = new List<MilitaryPlane>();
+            List<MilitaryPlane> militaryPlanes = GetMilitaryPlanes();
+            for (int i = 0; i < militaryPlanes.Count; i++)
+            {
+                MilitaryPlane plane = militaryPlanes[i];
+                if (plane.PlaneTypeIs() == MilitaryType.TRANSPORT)
+                {
+                    transportMilitaryPlanes.Add(plane);
+                }
+            }
+
+            return transportMilitaryPlanes;
+        }
+
+        public Airport SortByMaxDistance()
+        {
+            return new Airport(_planes.OrderBy(plane => plane.GetMaxFlightDistance()));
+        }
+
+        public Airport SortByMaxSpeed()
+        {
+            return new Airport(_planes.OrderBy(plane => plane.GetMaxSpeed()));
+        }
+
+        public Airport SortByMaxLoadCapacity()
+        {
+            return new Airport(_planes.OrderBy(plane => plane.GetMaxLoadCapacity()));
+        }
+
+
+        public IEnumerable<Plane> GetPlanes()
+        {
+            return _planes;
+        }
+
+        public override string ToString()
+        {
+            return "Airport{" +
+                    "planes=" + string.Join(", ", _planes.Select(plane => plane.GetModel())) +
+                    '}';
+        }
+    }
+}
---
+++ CleanCode/Net/Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,43 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        private readonly MilitaryType _type;
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            _type = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MilitaryPlane plane &&
+                  base.Equals(obj) &&
+                  _type == plane._type;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + _type.GetHashCode();
+            return hashCode;
+        }
+
+        public MilitaryType PlaneTypeIs()
+        {
+            return _type;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", type=" + _type +
+                    '}');
+        }        
+    }
+}
